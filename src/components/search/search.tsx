7737141976b7e4b411f06cdd2484675863238606
Allahverdi <isaevallahverdi@gmail.com>
2023-06-08 03:16:41 +0400

    changes happened

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -7,49 +7,49 @@ import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 
 const Search: FC = () => {
-    const [inputValue, setInputValue] = useState<string | null>();
-    const [searchSelectParams, setSearchSelectParams] = useSearchParams();
+  const [inputValue, setInputValue] = useState<string | null>();
+  const [searchSelectParams, setSearchSelectParams] = useSearchParams();
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setInputValue(event.target.value);
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
 
-        if (inputValue?.trim()) {
-            searchSelectParams.set("q", event.target.value);
-            setSearchSelectParams(searchSelectParams);
-        }
-        if (searchSelectParams.get("q") == "") {
-            searchSelectParams.delete("q");
-            setSearchSelectParams(searchSelectParams);
-        }
-    };
+    if (inputValue?.trim()) {
+      searchSelectParams.set("q", event.target.value);
+      setSearchSelectParams(searchSelectParams);
+    }
+    if (searchSelectParams.get("q") == "") {
+      searchSelectParams.delete("q");
+      setSearchSelectParams(searchSelectParams);
+    }
+  };
 
-    return (
-        <Box
-            sx={{
-                display: "flex",
-                justifyContent: "center",
-                marginBottom: "3px",
-            }}
-        >
-            <FormControl variant="standard" sx={{ width: 400, borderRadius: "24px" }}>
-                <TextField
-                    placeholder="Search city, country or location"
-                    onChange={handleChange}
-                    id="outlined-size-small"
-                    size="small"
-                    type="search"
-                    sx={{ borderRadius: "24px" }}
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <SearchIcon />
-                            </InputAdornment>
-                        ),
-                    }}
-                />
-            </FormControl>
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        marginBottom: "3px",
+      }}
+    >
+      <FormControl variant="standard" sx={{ width: 400, borderRadius: "24px" }}>
+        <TextField
+          placeholder="Search city, country or location"
+          onChange={handleChange}
+          id="outlined-size-small"
+          size="small"
+          type="search"
+          sx={{ borderRadius: "24px" }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </FormControl>
+    </Box>
+  );
 };
 
 export default Search;
--- src/components/today/today.tsx
+++ src/components/today/today.tsx
@@ -4,57 +4,57 @@ import Box from "@mui/material/Box";
 import { month, d } from "../../constants/months";
 
 const Today: FC = () => {
-    const showData = useAppSelector((state) => state.getDatas);
+  const showData = useAppSelector((state) => state.getDatas);
 
-    return (
-        <Box
-            sx={{
-                width: 400,
-                height: 180,
-                backgroundColor: "#2359B0",
-                color: "#fff",
-                margin: "auto",
-            }}
-        >
-            <Box
-                sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    marginBottom: "12px",
-                    padding: "0 20px",
-                }}
-            >
-                <Box>Today</Box>
-                <Box>
-                    {month}, {d.getDate()}
-                </Box>
-            </Box>
-
-            <Box
-                sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    textAlign: "center",
-                }}
-            >
-                {showData?.forecast?.forecastday?.map((item) => {
-                    return (
-                        <Box key={item.date}>
-                            <Box margin={"4px"}>{item?.date}</Box>
-                            <Box>
-                                <img
-                                    src={item?.day?.condition?.icon}
-                                    width={"30px"}
-                                    height={"30px"}
-                                />
-                            </Box>
-                            <Box>{Math.round(item?.day?.avgtemp_c)}°</Box>
-                        </Box>
-                    );
-                })}
-            </Box>
+  return (
+    <Box
+      sx={{
+        width: 400,
+        height: 180,
+        backgroundColor: "#2359B0",
+        color: "#fff",
+        margin: "auto",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "12px",
+          padding: "0 20px",
+        }}
+      >
+        <Box>Today</Box>
+        <Box>
+          {month}, {d.getDate()}
         </Box>
-    );
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          textAlign: "center",
+        }}
+      >
+        {showData?.forecast?.forecastday?.map((item) => {
+          return (
+            <Box key={item.date}>
+              <Box margin={"4px"}>{item?.date}</Box>
+              <Box>
+                <img
+                  src={item?.day?.condition?.icon}
+                  width={"30px"}
+                  height={"30px"}
+                />
+              </Box>
+              <Box>{Math.round(item?.day?.avgtemp_c)}°</Box>
+            </Box>
+          );
+        })}
+      </Box>
+    </Box>
+  );
 };
 
 export default Today;
